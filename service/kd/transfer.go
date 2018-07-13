c080ba15cb248799320af5ad2c9b189a10a9f454
chenwenfeng <chenwenfeng@wzdai.com>
2018-07-13 11:29:07 +0800

    trans time

--- service/kd/transfer.go
+++ service/kd/transfer.go
@@ -6,7 +6,7 @@ import (
 	"kd.explorer/tools/https"
 )
 
-const TransferListURL = "https://deposit.koudailc.com/credit/market-for-app-v2?appVersion=6.7.5&osVersion=11.300000&clientType=ios&deviceName=iPhone%20X&page=1&pageSize=5&sortRuleType=2"
+const TransferListURL = "https://deposit.koudailc.com/credit/market-for-app-v2?appVersion=6.7.5&osVersion=11.300000&clientType=ios&deviceName=iPhone%20X&page=1&pageSize=2&sortRuleType=2"
 const DefaultUserKEY = "cwf"
 const TransLoginSuccessSTATUS = 1
 const RetryCNT = 3
