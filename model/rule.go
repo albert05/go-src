0610cc723de1f95b7419d8eb1e923434687a435e
chenwenfeng <chenwenfeng@wzdai.com>
2018-07-16 16:17:35 +0800

    multi rule

---
+++ model/rule.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"kd.explorer/tools/mysql"
+	"fmt"
+)
+
+const RuleTable  = "rule_list"
+
+func FindRule(key string) mysql.MapModel {
+	sql := fmt.Sprintf("SELECT * FROM %s WHERE k='%s'", RuleTable, key)
+	rule, err := mysql.Conn.FindOne(sql)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return rule
+}
--- service/kd/analyse.go
+++ service/kd/analyse.go
@@ -6,6 +6,8 @@ import (
 	"kd.explorer/config"
 	"kd.explorer/tools/mail"
 	"kd.explorer/model"
+	"kd.explorer/tools/mysql"
+	"encoding/json"
 )
 
 // 告警线
@@ -19,7 +21,7 @@ const SecKillMinRATE = 30  // 30% 以上
 const SecKillMaxRestDAY = 150    // 60天以内
 
 var MonitorRule *Rule
-var SecKillRule *Rule
+var SecKillRules *Rules
 
 func init() {
 	MonitorRule = InitRule()
@@ -27,16 +29,31 @@ func init() {
 	MonitorRule.SetRate(MonitorMinRATE)
 	MonitorRule.SetRestdays(MonitorMaxRestDAY)
 
-	SecKillRule = InitRule()
-	SecKillRule.SetFee(config.SecKillFee)
-	SecKillRule.SetRate(config.SecKillRate)
-	SecKillRule.SetRestdays(config.SecKillRestDay)
+	SecKillRules = &Rules{}
+
+	var rule mysql.MapModel
+	if config.RuleKey != "" {
+		rule = model.FindRule(config.RuleKey)
+	}
+
+	if rule == nil {
+		secKillRule := InitRule()
+		secKillRule.SetFee(config.SecKillFee)
+		secKillRule.SetRate(config.SecKillRate)
+		secKillRule.SetRestdays(config.SecKillRestDay)
+		SecKillRules.R = []*Rule{secKillRule}
+	} else {
+		var r []*Rule
+		json.Unmarshal([]byte(rule.GetAttrString("rule")), &r)
+		SecKillRules.R = r
+	}
+
 }
 
 func (list *TransList) Analyse() {
 	monitorMsg := make([]string, 0)
 	for _, item := range list.List.Items {
-		if true == SecKillRule.Check(item) {
+		if true == SecKillRules.Check(item) {
 			if list.Cookie == "" {
 				item.SyncRunKill()
 			} else {
