7cf86b1bef13ab5db068024ee4ab1159262f98d5
albert <fengelom@qq.com>
2018-07-26 23:00:22 +0800

    base name

--- common/lock.go
+++ common/lock.go
@@ -46,5 +46,5 @@ func UnLock() bool {
 }
 
 func getLockName() string {
-	return GetLockPath() + fmt.Sprintf(path.Base(os.Args[0])+"_%s.lock", config.CurUser)
+	return GetLockPath() + fmt.Sprintf(config.ProNAME + path.Base(os.Args[0])+"_%s.lock", config.CurUser)
 }
